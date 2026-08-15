10839d24991798c5a2f45003c1ecfb3feaf98a8b
Nicolas Araujo <niaraujo0807@gmail.com>
2026-08-14 22:31:23 -0300

    feat: adiciona entidade campo

--- src/br/com/nicolas/cm/modelo/Campo.java
+++ src/br/com/nicolas/cm/modelo/Campo.java
@@ -1,5 +1,22 @@
 package br.com.nicolas.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Campo {
+	
+	private boolean minado = false;
+	private boolean aberto = false;
+	private boolean marcado = false;
+	
+	private List<Campo> vizinho = new ArrayList<>();
+	
+	private int linha;
+	private int coluna;
+	
+	Campo(int linha, int coluna){
+		this.linha = linha; 
+		this.coluna = coluna;
+	}
 
 }
